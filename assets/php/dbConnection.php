56c72930461e4395912bfeb725766f60bf3aed98
Jesus Badillo <badillosec26@gmail.com>
2024-03-27 20:21:27 -0600

    Update: adding funcions to validate the internal and external users

--- assets/php/dbConnection.php
+++ assets/php/dbConnection.php
@@ -1,3 +1,9 @@
 <?php
 
     $connection = mysqli_connect('localhost', 'root', '', 'oxxo');
+
+    //creation the email vlaidation function
+    function is_valid_email($str)
+    {
+        return (false !== filter_var($str, FILTER_VALIDATE_EMAIL));
+    }
--- assets/php/userLogin.php
+++ assets/php/userLogin.php
@@ -1,17 +1,27 @@
 <?php
 
+    //Start sesion cookie
     session_start();
 
+    //including db conection
     include("./dbConnection.php");
 
-    $uname = $_POST['uname'];
-    $psw = $_POST['psw'];
+    //Reciving data from login and cleaning the injections
+    $uname = isset($_POST['uname']) ? $connection ->real_escape_string($_POST['uname']) : null;
+    $psw = isset($_POST['psw']) ? $connection ->real_escape_string($_POST['psw']) : null;
     $hash = hash('sha256', $psw);
 
-    $query = "SELECT * FROM internalusers WHERE EmployeeNumber = '$uname' AND Hash = '$hash'";
+    //User Validation
+    if(is_valid_email($uname)){
+        $query = "SELECT * FROM externalusers WHERE Email = '$uname' AND Hash = '$hash'";
+    }else{
+        $query = "SELECT * FROM internalusers WHERE EmployeeNumber = '$uname' AND Hash = '$hash'";
+    }
 
+    //User authentification
     $loginValidation = mysqli_query($connection, $query);
 
+    //Login Error
     if(mysqli_num_rows($loginValidation) > 0){
         $_SESSION['user'] = $uname;
         header("location: ../../queryAssets.php");
