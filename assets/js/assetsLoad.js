1d85d435d7482792ea82d5cbd76c957a4246cade
Jesus Badillo <badillosec26@gmail.com>
2024-04-06 21:40:00 -0600

    Build: adding assestLoad js to search the assets

---
+++ assets/js/assetsLoad.js
@@ -0,0 +1,22 @@
+const input_btn = document.getElementById("search")
+
+    input_btn.addEventListener("click", getData)
+
+    function getData(){
+        let input = document.getElementById("assets-code").value;
+        let contet = document.getElementById("content")
+
+        let url = "assets/php/assetsLoad.php"
+
+        let formData = new FormData()
+        formData.append('assets-code', input)
+
+        fetch(url, {
+            method: "POST",
+            body: formData
+        }).then(response => response.json())
+        .then(data => {
+            contet.innerHTML = data
+        }).catch(err => console.log(err))
+
+    }
--- queryAssets.php
+++ queryAssets.php
@@ -84,30 +84,7 @@
             
             
         </form>
-        <script>
-
-            document.getElementById("search").addEventListener("click", getData)
-
-            function getData(){
-                let input = document.getElementById("assets-code").value;
-                let contet = document.getElementById("content")
-
-                let url = "assets/php/assetsLoad.php"
-
-                let formData = new FormData()
-                formData.append('assets-code', input)
-
-                fetch(url, {
-                    method: "POST",
-                    body: formData
-                }).then(response => response.json())
-                .then(data => {
-                    contet.innerHTML = data
-                }).catch(err => console.log(err))
-
-            }
-
-        </script>
 
     </body>
+    <script src="assets/js/assetsLoad.js"></script>
 </html>
